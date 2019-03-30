f7ef67d4a5160fe3fa150c09455073f3ca122a53
Ziad Abouelfarah <ziadabouelfarah2@gmail.com>
2019-03-30 22:14:18 +0100

    Some new stuff that doesn't work

--- app/src/main/java/com/abouelfarah/facefood/fragment/suggestion_fragment.kt
+++ app/src/main/java/com/abouelfarah/facefood/fragment/suggestion_fragment.kt
@@ -2,23 +2,26 @@ package com.abouelfarah.facefood.fragment
 
 import android.os.Build
 import android.os.Bundle
-import android.os.Handler
 import android.support.v4.app.Fragment
 import android.support.v7.widget.Toolbar
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.abouelfarah.facefood.R
+import com.abouelfarah.facefood.models.post
 import com.abouelfarah.facefood.models.suggetion
 import com.abouelfarah.facefood.suggestion_card
+import com.google.firebase.database.FirebaseDatabase
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.Item
 import com.xwray.groupie.ViewHolder
 import kotlinx.android.synthetic.main.add_suggestion.view.*
 import kotlinx.android.synthetic.main.fragment_suggestion.*
 import java.util.Random
+import android.os.Handler
+import android.widget.Toast
 
-class suggestion_fragment: Fragment() {
+class suggestion_fragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
@@ -43,8 +46,6 @@ class suggestion_fragment: Fragment() {
         }
 
         recyclerViewer_from_suggestion_fragment.adapter = adapter
-
-
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -53,14 +54,17 @@ class suggestion_fragment: Fragment() {
     }
 }
 
-
-class add_suggest():Item<ViewHolder>(){
+@Suppress("NAME_SHADOWING")
+class add_suggest() : Item<ViewHolder>() {
 
     override fun getLayout(): Int {
         return R.layout.add_suggestion
     }
 
     override fun bind(viewHolder: ViewHolder, position: Int) {
+
+        val name = viewHolder.itemView.name_from_add_suggestion.text.toString()
+        val idea = viewHolder.itemView.idea_from_add_suggestion.text.toString()
 
         viewHolder.itemView.cancel_button.setOnClickListener {
             viewHolder.itemView.name_from_add_suggestion.text.clear()
@@ -69,18 +73,48 @@ class add_suggest():Item<ViewHolder>(){
         }
 
         viewHolder.itemView.post_button.setOnClickListener {
+
+            var work:Int = 0
+            var msg :String ?= null
+
             viewHolder.itemView.post_button.text = "Posting..."
             viewHolder.itemView.post_button.isEnabled = false
             viewHolder.itemView.cancel_button.isEnabled = false
-            val handler = Handler()
-            handler.postDelayed(object:Runnable{
-                override fun run() {
-                    viewHolder.itemView.post_button.text = "Post"
-                    viewHolder.itemView.post_button.isEnabled = true
-                    viewHolder.itemView.cancel_button.isEnabled = true
-                }
-            }, 3000)
-        }
 
+
+            val name = viewHolder.itemView.name_from_add_suggestion.text.toString()
+            val idea = viewHolder.itemView.idea_from_add_suggestion.text.toString()
+
+            val handler = Handler()
+            handler.postDelayed(Runnable {
+
+                val currentTime = System.currentTimeMillis()
+                val ref = FirebaseDatabase.getInstance().getReference("/suggestion").push()
+                val data = post(name, idea, currentTime)
+                ref.setValue(data).addOnFailureListener {
+                    work = 0
+                    msg = it.message.toString()
+                }.addOnSuccessListener {
+                    work = 1
+                }
+
+
+                viewHolder.itemView.post_button.text = "Post"
+                viewHolder.itemView.post_button.isEnabled = true
+                viewHolder.itemView.cancel_button.isEnabled = true
+
+            }, 2000)
+
+            if(work == 0){
+                Toast.makeText(it.context, "MA5EDAMX SAFI", Toast.LENGTH_SHORT).show()
+            }else if(work == 1){
+                Toast.makeText(it.context, "Posted", Toast.LENGTH_SHORT).show()
+            }
+
+
+
+        }
     }
+
+
 }
---
+++ app/src/main/java/com/abouelfarah/facefood/models/post.kt
@@ -0,0 +1,5 @@
+package com.abouelfarah.facefood.models
+
+class post(val name : String, val idea:String, val timestamp:Long){
+    constructor():this("", "", 1)
+}
